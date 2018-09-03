91e0f7d35146d108085dbadbedaeaad1c1ae3864
SerDjant927 <sergey.poroshin927@gmail.com>
2018-09-03 16:56:07 +0700

    problems with script 2

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+ 	
+ if($('.slider').length > 0) {
+
+ }
+ sliderInit();
+
+ function sliderInit(){
+ 
+ var count = $('.slider li').length;
+ $('.slider').append('<div class="nav"></div>');
+ for(var i = 0; i < count; i++){
+ $('.nav').find('.nav').append('<span>'+i+'</span>');
+}
+ console.log(count);
+
+ 	}
+ });
