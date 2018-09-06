4137515fc23a85acb1beb4b062ccb1f10d921b79
SerDjant927 <sergey.poroshin927@gmail.com>
2018-09-06 16:44:10 +0700

    06.09.18_4

--- js/script.js
+++ js/script.js
@@ -1,32 +1,12 @@
-
-
-$(document).ready(function() {
-    var leftUIEl = $('.slider_arrow_left');
-    var rightUIEl = $('.slider_arrow_right');
-    var elementsList = $('.slick-slider');
- 
-    var pixelsOffset = 225;
-    var currentLeftValue = 0;
-    var elementsCount = elementsList.find('li').length;
-    var minimumOffset = - ((elementsCount - 1) * pixelsOffset);
-    var maximumOffset = 0;
- 
-    leftUIEl.click(function() {        
-        if (currentLeftValue != maximumOffset) {
-            currentLeftValue += 225;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }        
-    });
- 
-    rightUIEl.click(function() {        
-        if (currentLeftValue != minimumOffset) {
-            currentLeftValue -= 225;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }        
-    });
+$(document).ready(function(){
+  $('.menu_link').on('click', function(){
+	var id = $(this).data('link');
+	var block = $('body').find('[data-block="'+id+'"]');
+	var top = block.offset().top;
+    $('html, body').animate({scrollTop: top}, 1500);
+	return false;
+})
 });
-
-
 
 
 
